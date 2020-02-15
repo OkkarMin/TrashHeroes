81a1e81ab199cbd979919853c8ae3b71f43d80b5
Okkar Min <phoehtaungwin@gmail.com>
2020-02-15 20:53:09 +0800

    Added GPIO setup to UltrasoundSensor.py

--- UltrasoundSensor.py
+++ UltrasoundSensor.py
@@ -1,8 +1,10 @@
 from gpiozero import DistanceSensor
+import RPi.GPIO as GPIO
 
 
 class UltrasoundSensor(object):
     def __init__(self):
+        GPIO.setmode(GPIO.BCM)
         self.sensor = DistanceSensor(trigger=23, echo=24)
 
     def get_distance(self):
