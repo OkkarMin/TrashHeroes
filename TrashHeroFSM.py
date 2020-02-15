c2f17609a7c53d610c15f5c873c59e9c45dbb92c
Okkar Min <phoehtaungwin@gmail.com>
2020-02-15 10:54:08 +0800

    Added Transitions
    For TrashHero FSM

--- TrashHeroFSM.py
+++ TrashHeroFSM.py
@@ -11,3 +11,33 @@ class TrashHero(object):
         "left_flap_open",
         "right_flap_open",
     ]
+
+    transitions = [
+        {"trigger": "change_to_setup", "source": "initial", "destination": "setup"},
+        {"trigger": "change_to_ready", "source": "setup", "destination": "ready"},
+        {
+            "trigger": "change_to_detecting_object",
+            "source": "ready",
+            "destination": "detecting_object",
+        },
+        {
+            "trigger": "change_to_object_detected",
+            "source": "detecting_object",
+            "destination": "object_detected",
+        },
+        {
+            "trigger": "change_to_left_flap_open",
+            "source": "object_detected",
+            "destination": "left_flap_open",
+        },
+        {
+            "trigger": "change_to_right_flap_open",
+            "source": "object_detected",
+            "destination": "right_flap_open",
+        },
+        {
+            "trigger": "change_to_ready",
+            "source": ["left_flap_open", "right_flap_open"]
+            "destination": "ready",
+        },
+    ]
